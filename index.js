7cd20b3aa32cac20eec97f697f70163b1cd9393e
Seth David Gyimah <esec26@gmail.com>
2022-06-11 12:17:11 +0000

    phase-1 scope-lab

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Write your solution in this file!
+var customerName = "bob"
+const leastFavoriteCustomer = "who knows"
+
+
+function upperCaseCustomerName(){
+    customerName = customerName.toUpperCase()
+   // return a
+}
+
+function setBestCustomer(){
+    bestCustomer = "not bob"
+}
+
+function overwriteBestCustomer(){
+    bestCustomer = "maybe bob"
+}
+
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = "I know"
+}
